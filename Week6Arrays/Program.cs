9cb531d15283efbf3666a7065f70005c12bb2597
ayseekinerdur <ekinerdurr@gmail.com>
2023-04-16 15:43:02 +0300

    week 6

---
+++ Week6Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string[] fruitBasket = { "apples", "bananas", "oranges" };
+
+Console.WriteLine($"There are {fruitBasket.Length} fruit in your basket.");
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+
+fruitBasket[0] = "pine-apple";
+fruitBasket[2] = "peaches";
+
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+
+for(int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
